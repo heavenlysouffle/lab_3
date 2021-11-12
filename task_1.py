6337173f9e3ba51e54aa9360cca1b5ff432e37c5
pivo <84128815+heavenlysouffle@users.noreply.github.com>
2021-11-12 18:50:08 +0200

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -66,31 +66,30 @@ class Event:
         """Method to generate a ticket"""
         if Event._ticket_number >= Event.__tickets_max:
             raise ValueError("All tickets are sold")
-        if not isinstance(customer, Customer):
-            raise TypeError
         info = []
         if (self.__date - date).days <= 356:
             if customer.student_ind:
-                ticket = Student()
+                ticket = Student(customer)
                 info.append({"Type": "Student"})
             else:
                 if (self.__date - date).days >= 60:
-                    ticket = Advance()
+                    ticket = Advance(customer)
                     info.append({"Type": "Advance"})
                 elif (self.__date - date).days <= 10:
-                    ticket = Late()
+                    ticket = Late(customer)
                     info.append({"Type": "Late"})
                 else:
-                    ticket = Regular()
+                    ticket = Regular(customer)
                     info.append({"Type": "Regular"})
         else:
             raise ValueError("Invalid date")
-        info.append({"Surname": customer.surname})
-        info.append({"Name": customer.name})
+        info.append({"Surname": ticket.customer.surname})
+        info.append({"Name": ticket.customer.name})
         info.append({"Price": ticket.price})
-        Event.__tickets_data.append({Event._ticket_number: info})
+        Event.__tickets_data.append({ticket.id: info})
 
-    def serialization(self, filepath):
+    @staticmethod
+    def serialization(filepath):
         """Method to write ticket information to json file"""
         if not os.path.exists(filepath):
             raise OSError("File was not found")
@@ -101,8 +100,34 @@ class Event:
         with open(filepath, "w") as json_file:
             json.dump(Event.__tickets_data, json_file, indent=4)
 
-    def find_ticket(self, key, filepath):
-        """Method to extract ticket by a number"""
+    @staticmethod
+    def deserialization(filepath):
+        """Method to extract information from json file"""
+        if not os.path.exists(filepath):
+            raise OSError("File was not found")
+        if os.path.getsize(filepath):
+            pass
+        else:
+            raise OSError("File is empty")
+        with open(filepath, "r") as json_file:
+            return json.load(json_file)
+
+    def find_id(self, ticket_id):
+        """Method to find a ticket by its number and return as string"""
+        if not isinstance(ticket_id, int):
+            raise TypeError
+        if ticket_id <= 0:
+            raise ValueError("Ticket ID must be above zero")
+        if ticket_id > Event._ticket_number:
+            return f'There\'s no such ID'
+        for record in self.__tickets_data:
+            for key in record.keys():
+                if key == ticket_id:
+                    return record[key]
+
+    @staticmethod
+    def find_ticket(key, filepath):
+        """Method to extract ticket by a number from json file"""
         if not os.path.exists(filepath):
             raise OSError("File was not found")
         if os.path.getsize(filepath):
@@ -120,14 +145,15 @@ class Event:
                     if i == key:
                         return dict
 
-    def return_str(self, key, filepath):
-        """Method that returns info about a ticket picked by a number as string"""
-        data = Event.find_ticket(self, key, filepath)
+    @staticmethod
+    def return_str(key, filepath):
+        """Method that returns info about a ticket picked by a number as string from json file"""
+        data = Event.find_ticket(key, filepath)
         return f'{data[key][0]["Type"]} {data[key][1]["Surname"]} {data[key][2]["Name"]} {data[key][3]["Price"]}'
 
     def price(self, key, filepath):
-        """Method that returns price of a ticked picked by a number"""
-        data = Event.find_ticket(self, key, filepath)
+        """Method that returns price of a ticked picked by a number from json file"""
+        data = Event.find_ticket(key, filepath)
         for dict in data[key]:
             for i in dict.keys():
                 if i == "Price":
@@ -159,43 +185,102 @@ class Event:
 
 
 class Regular(Event):
-    def __init__(self):
+    """Class that builds regular ticket.
+    Contains ticket's price (the same as basic price for event), number and information about a customer"""
+
+    def __init__(self, customer):
+        if not isinstance(customer, Customer):
+            raise TypeError
         self.__price = self.basic_price
         Event._ticket_number += 1
+        self.__id = Event._ticket_number
+        self.__customer = customer
 
     @property
     def price(self):
         return self.__price
 
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def customer(self):
+        return self.__customer
 
 class Advance(Event):
-    def __init__(self):
+    """Class that builds advanced ticket.
+    Contains ticket's price (sale 40% of basic price for event), number and information about a customer"""
+
+    def __init__(self, customer):
+        if not isinstance(customer, Customer):
+            raise TypeError
         self.__price = 0.6 * self.basic_price
         Event._ticket_number += 1
+        self.__id = Event._ticket_number
+        self.__customer = customer
 
     @property
     def price(self):
         return self.__price
+
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def customer(self):
+        return self.__customer
 
 
 class Student(Event):
-    def __init__(self):
+    """Class that builds student ticket.
+    Contains ticket's price (sale 50% of basic price for event), number and information about a customer"""
+
+    def __init__(self, customer):
+        if not isinstance(customer, Customer):
+            raise TypeError
         self.__price = 0.5 * self.basic_price
         Event._ticket_number += 1
+        self.__id = Event._ticket_number
+        self.__customer = customer
 
     @property
     def price(self):
         return self.__price
+
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def customer(self):
+        return self.__customer
 
 
 class Late(Event):
-    def __init__(self):
+    """Class that builds late ticket.
+    Contains ticket's price (additional 10% of basic price for event), number and information about a customer"""
+
+    def __init__(self, customer):
+        if not isinstance(customer, Customer):
+            raise TypeError
         self.__price = 1.1 * self.basic_price
         Event._ticket_number += 1
+        self.__id = Event._ticket_number
+        self.__customer = customer
 
     @property
     def price(self):
         return self.__price
+
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def customer(self):
+        return self.__customer
 
 
 event1 = Event("Mondstadt", date(2021, 12, 31), 10, 250)
@@ -207,3 +292,4 @@ event1.serialization("tickets.JSON")
 print(event1.find_ticket("2", "tickets.JSON"))
 print(event1.price("1", "tickets.JSON"))
 print(event1.return_str("2", "tickets.JSON"))
+print(event1.find_id(1))
